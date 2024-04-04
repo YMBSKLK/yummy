a4fcb06508186176997a3f0a5ee61f3782f31378
sun_hming <sun_hming@cvicse.com>
2024-04-04 09:44:23 +0800

    Initial commit

---
+++ application/src/main/java/cn/maybe/server/YummyApplication.java
@@ -0,0 +1,36 @@
+package cn.maybe.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+/**
+ * 项目启动类
+ *
+ * @author sun_hming
+ * @date 2024/04/04 09:21
+ */
+@SpringBootConfiguration
+@ComponentScan("cn.maybe.server")
+public class YummyApplication {
+
+    private static final String SPRING_CONFIG_NAME_KEY = "--spring.config.name";
+    private static final String DEFAULT_SPRING_CONFIG_PARAM = SPRING_CONFIG_NAME_KEY + "=" + "yummy";
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyApplication.class, updateArguments(args));
+    }
+
+    private static String[] updateArguments(String[] args) {
+        if (Arrays.stream(args).noneMatch(arg -> arg.startsWith(SPRING_CONFIG_NAME_KEY))) {
+            String[] modifiedArgs = new String[args.length + 1];
+            System.arraycopy(args, 0, modifiedArgs, 0, args.length);
+            modifiedArgs[args.length] = DEFAULT_SPRING_CONFIG_PARAM;
+            return modifiedArgs;
+        }
+        return args;
+    }
+
+}
